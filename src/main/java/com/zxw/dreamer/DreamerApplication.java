62a7da7c649ff58e87f2f0b7ae63af52822ead38
zhangxiangwei <zhangxiangwei@w-ew.com>
2021-10-21 17:01:20 +0800

    master分支提交测试冲突

--- src/main/java/com/zxw/dreamer/DreamerApplication.java
+++ src/main/java/com/zxw/dreamer/DreamerApplication.java
@@ -10,6 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DreamerApplication {
 
+    /// 22222
     public static void main(String[] args) {
         SpringApplication.run(DreamerApplication.class, args);
     }
