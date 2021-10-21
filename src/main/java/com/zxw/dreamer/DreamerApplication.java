94543f9604287b77dfbbdedcc9f5df56fa07cc2b
zhangxiangwei <zhangxiangwei@w-ew.com>
2021-10-21 17:16:29 +0800

    测试提交

--- src/main/java/com/zxw/dreamer/DreamerApplication.java
+++ src/main/java/com/zxw/dreamer/DreamerApplication.java
@@ -10,10 +10,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DreamerApplication {
 
-    /// 22222
-//111
-    //2222
-    //  22223333
     public static void main(String[] args) {
         SpringApplication.run(DreamerApplication.class, args);
     }
