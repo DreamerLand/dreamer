314aac453ff8a67fb3ebc226f7f3e1146306e0cf
zhangxiangwei <zhangxiangwei@w-ew.com>
2021-10-21 16:56:39 +0800

    dev分支提交

--- src/main/java/com/zxw/dreamer/DreamerApplication.java
+++ src/main/java/com/zxw/dreamer/DreamerApplication.java
@@ -1,6 +1,5 @@
 package com.zxw.dreamer;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
